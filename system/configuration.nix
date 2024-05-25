1836b42566a5f911e8b795ba24b36b7da7e8829a
polowiper <53773040+polowiper@users.noreply.github.com>
2024-05-25 15:35:42 +0200

    update

--- system/configuration.nix
+++ system/configuration.nix
@@ -30,7 +30,7 @@ in{
   nix.settings = {
     experimental-features = [ "nix-command" "flakes" ];
     substituters = ["https://hyprland.cachix.org" "https://nix-community.cachix.org"];
-    trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
+    trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="];
     };
 
   # Enable networking
